7e4d6ab4625c8a484909a73d3510497fbce8b24e
xaviguasch <xaviguasch@gmail.com>
2020-02-11 18:30:05 +0100

    added selection of seats on the UI

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('working!!!')
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+
+const ticketPrice = parseInt(movieSelect.value)
+
+container.addEventListener('click', e => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected')
+  }
+})
